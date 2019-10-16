d459573831aa99be3c006e46683b19006e7389d9
Maxim Pogrebnyak <pogrebnyakmaksim@mail.ru>
2019-10-16 20:23:58 +0300

    added logo

--- site/views/layout.php
+++ site/views/layout.php
@@ -1,6 +1,8 @@
 <div id="layout">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Network Technologies</a>
+        <a class="navbar-brand" href="#">
+            <img src="styles/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Network Technologies</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
